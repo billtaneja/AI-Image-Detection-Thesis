46775756538398068f4d36ee8ede3956f755914d
billtaneja <taneja.work@gmail.com>
2025-12-20 13:12:44 +0100

    Update ai_vs_real_gradio_app (1).py

--- ai_vs_real_gradio_app (1).py
+++ ai_vs_real_gradio_app (1).py
@@ -1,59 +1,39 @@
-
-# Install Gradio
-# --------------
-# !pip install gradio
-
-# Load Your Model
-# ---------------
-
 from tensorflow import keras
 
-# Load trained model (update path as needed)
+# Loading trained model
 model = keras.models.load_model("/Users/yashtaneja/Desktop/Yash_Thesis/coding and model/AI_vs_Real_Model.h5")
 
-# Define labels
 class_names = ["Real", "AI-Generated"]
-
-# Define Preprocessing + Prediction Function
-# ------------------------------------------
 
 import numpy as np
 from PIL import Image
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 
 def predict_image(img):
-    # Ensure RGB (drop alpha channel if present)
+   
     img = img.convert("RGB")
 
-    # Resize to match model input
     img = img.resize((224, 224))
-
-    # Convert to numpy
     img_array = np.array(img)
 
-    # Expand dims for batch
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Apply same preprocessing as training
     img_array = preprocess_input(img_array)
 
-    # Prediction
+    # Predictiing
     pred = model.predict(img_array)
 
-    if pred.shape[1] == 1:  # sigmoid output
+    if pred.shape[1] == 1:  
         predicted_class = int(pred[0][0] > 0.5)
         confidence = pred[0][0] if predicted_class == 1 else 1 - pred[0][0]
-    else:  # softmax
+    else: 
         predicted_class = np.argmax(pred, axis=1)[0]
         confidence = pred[0][predicted_class]
 
     return {class_names[predicted_class]: float(confidence)}
 
-# Create Gradio Interface
-# -----------------------
-
+# Gradio!!!
 import gradio as gr
-
 interface = gr.Interface(
     fn=predict_image,
     inputs=gr.Image(type="pil"),
